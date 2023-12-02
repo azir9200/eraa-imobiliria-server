3ef01c9c7008e597e58c516a85f5e2a76f32f605
Azir@9200 <aziruddin83@hotmail.com>
2023-12-02 02:47:15 +0000

    created jwt api for user control

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -27,15 +28,82 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const usersCollection = client.db('eraaRealDb').collection('users');
     const houseCollection = client.db('eraaRealDb').collection('allHouses');
     const reviewsCollection = client.db('eraaRealDb').collection('reviews');
     const wishListCollection = client.db('eraaRealDb').collection('wishList');
 
+    //jwt related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h'
+      });
+      res.send({ token });
+    })
 
+    //middleware
+    const varifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers.Authorization);
+      if (!req.headers.Authorization) {
+        return res.status(401).send({ message: 'forbidden access' });
+      }
+      const token = req.headers.Authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+      // next();
+    }
+
+    // user related api
+    app.get('/users', varifyToken, async (req, res) => {
+      console.log(req.headers);
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      //insert email if user not signupn yet  1.email unique/upsert/simple
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exist', insertedId: null });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
+    //admin related api
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updateDoc)
+      res.send(result);
+    })
+
+    // user delete api
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    // allHouse collection api
     app.get('/allHouses', async (req, res) => {
       const result = await houseCollection.find().toArray();
       res.send(result);
     })
+
 
     app.get('/reviews', async (req, res) => {
       const result = await reviewsCollection.find().toArray();
@@ -51,7 +119,9 @@ async function run() {
 
     //all wishlist collection
     app.get('/wishList', async (req, res) => {
-      const result = await wishListCollection.find().toArray()
+      const email = req.query.email;
+      const query = { email: email }
+      const result = await wishListCollection.find(query).toArray()
       res.send(result);
     })
 
@@ -60,6 +130,14 @@ async function run() {
       const wishList = req.body;
       const result = await wishListCollection.insertOne(wishList)
       res.send(result);
+    })
+    // delete operation
+    app.delete('/wishList/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await wishListCollection.deleteOne(query);
+      res.send(result);
+
     })
 
     // Send a ping to confirm a successful connection
